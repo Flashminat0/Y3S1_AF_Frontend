ee853cdbeda1e9fb0e14c4e702034bf53a8027a1
Flashminat0 <wyasaslowe@gmail.com>
2022-04-07 13:38:51 +0530

    Initial commit with nextjs with custom server

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
-const express = require('express')
-const next = require('next')
+const express = require("express");
+const next = require("next");
+const {createProxyMiddleware} = require("http-proxy-middleware");
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== "production";
+const app = next({dev});
+const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express()
+app.prepare()
+    .then(() => {
+        const server = express();
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
+        server.use(
+            "/api",
+            createProxyMiddleware({
+                target: "http://localhost:8000",
+                changeOrigin: true,
+            }),
+        );
 
-  server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+        // apply proxy in dev mode
+        server.all("*", (req, res) => {
+            return handle(req, res);
+        });
+
+
+        server.listen(3000, (err) => {
+            if (err) throw err;
+            console.log("> Ready on http://localhost:3000");
+        });
+    })
+    .catch((err) => {
+        console.log("Error", err);
+    });
