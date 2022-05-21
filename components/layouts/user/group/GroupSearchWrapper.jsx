7e0130aca23bf27163438967dd66368a08beb5b9
swee76 <oshinikithma1213@gmail.com>
2022-05-21 10:45:30 +0530

    Group search wrapper

---
+++ components/layouts/user/group/GroupSearchWrapper.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GroupSearchWrapper = ({children}) => {
+    return (
+        <div className={"p-6"}>
+            {children}
+        </div>
+    );
+};
+
+export default GroupSearchWrapper;
