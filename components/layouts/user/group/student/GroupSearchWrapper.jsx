5aacfb9b459f93271d71304debfaa0fb32862260
Flashminat0 <wyasaslowe@gmail.com>
2022-05-22 00:19:37 +0530

    sending to heroku

--- components/layouts/user/group/student/GroupSearchWrapper.jsx
+++ components/layouts/user/group/student/GroupSearchWrapper.jsx
@@ -5,7 +5,6 @@ import {OutlinedInput} from "@mui/material";
 const GroupSearchWrapper = ({children}) => {
     return (
         <div className={"px-6 py-6"}>
-
             <OutlinedInput fullWidth={true} className={"h-10 mb-2"} endAdornment={<FiSearch className={"w-6 h-6"}/>}/>
             <hr/>
             {children}
