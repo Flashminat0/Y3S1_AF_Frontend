1f5a6343890bb1e29b5e4bcf09a11d55d3712bb5
swee76 <oshinikithma1213@gmail.com>
2022-05-21 16:25:48 +0530

    Single Group box, Groups List Group Search wrapper of admin

---
+++ components/boxex/lists/group/admin/GroupsList.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import GroupSearchWrapper from "../../../../layouts/user/group/admin/GroupSearchWrapper";
+import SingleGroupBox from "./SingleGroupBox";
+
+const studentGroupsStaticData = [{
+    groupName: "Noobcooders",
+    groupLeader: "John Smith",
+    groupLeaderRegNo: "IT14256789",
+    maxNo: 4,
+    currentNo: 3
+}, {
+    groupName: "thorn",
+    groupLeader: "Mary Snatiya",
+    groupLeaderRegNo: "IT20256749",
+    maxNo: 8,
+    currentNo: 1
+}, {
+    groupName: "BugFixers",
+    groupLeader: "Jonas Smith",
+    groupLeaderRegNo: "IT24156785",
+    maxNo: 5,
+    currentNo: 3
+}, {
+    groupName: "darkcodes",
+    groupLeader: "Leo Max",
+    groupLeaderRegNo: "IT10256350",
+    maxNo: 4,
+    currentNo: 3
+}
+];
+
+const GroupsList = () => {
+    const [studentGroups, setStudentGroups] = useState(studentGroupsStaticData);
+
+    return (
+        <div>
+            <GroupSearchWrapper>
+                {studentGroups.map((studentGroup) => (
+                    <SingleGroupBox groupName={studentGroup.groupName} groupLeader={studentGroup.groupLeader}
+                                    groupLeaderRegNo={studentGroup.groupLeaderRegNo} maxNo={studentGroup.maxNo}
+                                    currentNo={studentGroup.currentNo}/>
+                ))}
+            </GroupSearchWrapper>
+        </div>
+    );
+};
+
+export default GroupsList;
