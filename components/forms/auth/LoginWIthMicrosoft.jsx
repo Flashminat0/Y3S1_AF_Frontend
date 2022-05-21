f79c18409920d1617783a0dd5d0bf648e64abf2e
Flashminat0 <wyasaslowe@gmail.com>
2022-05-21 14:31:11 +0530

    common modal

--- components/forms/auth/LoginWIthMicrosoft.jsx
+++ components/forms/auth/LoginWIthMicrosoft.jsx
@@ -38,7 +38,6 @@ const LoginWIthMicrosoft = () => {
                 const accessToken = credential.accessToken;
                 const idToken = credential.idToken;
 
-                setCredentials(credential);
 
                 // Get the user's profile data from the Google API
                 axios.get('https://graph.microsoft.com/v1.0/me', {
@@ -47,8 +46,28 @@ const LoginWIthMicrosoft = () => {
                     }
                 }).then(response => {
                     return response.data;
-                }).then(data => {
-                    setProfileData(data);
+                }).then(async data => {
+                    console.log("data", data);
+
+                    await axios.post('/api/register', {
+                        name: data.displayName,
+                        userId: data.id,
+                    }).then(res => {
+                        setCredentials({
+                            ...credential,
+                            _id: res.data.user._id,
+                            steamToken: res.data.user.steamToken,
+                            role: res.data.user.role,
+                            userId: res.data.user.userId,
+                        });
+
+
+                        console.log(res);
+                    }).catch(err => {
+                        console.log(err);
+                    })
+
+                    // setProfileData(data);
                 })
 
             })
@@ -58,32 +77,18 @@ const LoginWIthMicrosoft = () => {
     }
 
 
-    const getMicrosoftRedirectResult = () => {
-        console.log(profileData);
-        console.log(credentials);
-    }
-
-    const signOutFromMicrosoft = () => {
-        signOut(auth).then(() => {
-            // Sign-out successful.
-        }).catch((error) => {
-            // An error happened.
-        });
-    }
-
-
     return (
         <div className={`grid place-content-center`}>
             <Button
                 color={'secondary'} variant="outlined" onClick={signInWithMicrosoft}
-                    startIcon={<svg className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 48 48" width="2.25rem" height="2.25rem">
-                        <path fill="#ff5722" d="M6 6H22V22H6z" transform="rotate(-180 14 14)"/>
-                        <path fill="#4caf50" d="M26 6H42V22H26z" transform="rotate(-180 34 14)"/>
-                        <path fill="#ffc107" d="M26 26H42V42H26z" transform="rotate(-180 34 34)"/>
-                        <path fill="#03a9f4" d="M6 26H22V42H6z" transform="rotate(-180 14 34)"/>
-                    </svg>
-                    }>Sign in with Microsoft
+                startIcon={<svg className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 48 48" width="2.25rem" height="2.25rem">
+                    <path fill="#ff5722" d="M6 6H22V22H6z" transform="rotate(-180 14 14)"/>
+                    <path fill="#4caf50" d="M26 6H42V22H26z" transform="rotate(-180 34 14)"/>
+                    <path fill="#ffc107" d="M26 26H42V42H26z" transform="rotate(-180 34 34)"/>
+                    <path fill="#03a9f4" d="M6 26H22V42H6z" transform="rotate(-180 14 34)"/>
+                </svg>
+                }>Sign in with Microsoft
             </Button>
         </div>
     );
