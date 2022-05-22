d78a5637b6b17289737d0b37d9e127721117105f
Sameera <sameeranicho@gmail.com>
2022-05-22 20:06:14 +0530

    warningsClear

--- components/dashboard/AdminDashboard.jsx
+++ components/dashboard/AdminDashboard.jsx
@@ -2,30 +2,26 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useLocalStorage } from "@mantine/hooks";
 import axios from "axios";
-import UserTable from '../common/table/userTable'
+import UserTable from "../common/table/userTable";
 
 const AdminDashboard = () => {
   const router = useRouter();
   const [profileData, setProfileData] = useState({});
-  const [userData, serUserData] = useState([{}]);
+  const [userData, setUserData] = useState([{}]);
   const [credentials, setCredentials] = useLocalStorage({
     key: "y3s1-af-credentials",
     defaultValue: {},
   });
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios("http://localhost:8000/api/userlist");
-      serUserData(result.data.result);
-    };
-    fetchData();
 
-    if (credentials.role !== "admin") {
-      router.push("/");
-    }
-  }, [credentials.type]);
-  return <div>
-      <UserTable users={userData}/>
-  </div>;
+  useEffect(() => {
+    axios.get("/api/users/userlist").then((result) => {
+      setUserData(result.data.result);
+    });
+  }, []);
+
+  return <div>{
+     <UserTable users={userData} /> 
+     }</div>;
 };
 
 export default AdminDashboard;
