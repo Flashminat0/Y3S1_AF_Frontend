414ee43b75c3920b1c09606494aa10c5c063a7d9
Flashminat0 <Flashminat0@users.noreply.github.com>
2022-05-22 19:56:59 +0000

    Prettified Code!

--- components/approvals/visuals/OkIcon.jsx
+++ components/approvals/visuals/OkIcon.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import {motion} from "framer-motion";
+import { motion } from 'framer-motion'
 
 const OkIcon = () => {
     return (
         <motion.div
-            initial={{opacity: 0, y: -20, scale: 1}}
-            animate={{opacity: 1, y: [0, 5, -5, 5, -5, 0]}}
-            transition={{delay: 0.5, duration: 0.5}}
+            initial={{ opacity: 0, y: -20, scale: 1 }}
+            animate={{ opacity: 1, y: [0, 5, -5, 5, -5, 0] }}
+            transition={{ delay: 0.5, duration: 0.5 }}
         >
             <img
                 src="https://img.icons8.com/color/480/000000/ok--v1.png"
