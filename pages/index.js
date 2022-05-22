fc04731416a2556072f721bed7ed6fc66490919e
Flashminat0 <wyasaslowe@gmail.com>
2022-05-22 09:49:29 +0530

    added next-HPM

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,28 @@ import FileUploadTest from "../components/forms/FileUploadTest";
 import LoginWIthMicrosoft from "../components/forms/auth/LoginWIthMicrosoft";
 import Header from "../components/common/header";
 import {useDocumentTitle} from '@mantine/hooks';
+import Button from "@mui/material/Button";
+import axios from "axios";
 
 export default function Home() {
     useDocumentTitle("Home");
 
 
+    const testAPI = () => {
+        axios.get('/api/test').then(res => {
+            console.log(res);
+        })
+    }
+
     return (
         <div className={`font-sans`}>
             <Header/>
-            <li>
-                <Link href="/b  " as="/a">
-                    <a>a</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="/a" as="/b">
-                    <a>bs</a>
-                </Link>
-            </li>
+            <Button
+                onClick={testAPI}
+                color={'primary'}>
+                TEST
+            </Button>
+
             <h1 className="text-3xl font-bold underline text-lg bg-red-100 text-red-500">
                 Hello world!
             </h1>
