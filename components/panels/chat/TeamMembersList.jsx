db72187a57453ebfa8fda3b38dda008b7713e8c1
Flashminat0 <wyasaslowe@gmail.com>
2022-05-23 00:50:46 +0530

    team members plaque

--- components/panels/chat/TeamMembersList.jsx
+++ components/panels/chat/TeamMembersList.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
-const TeamMembersList = ({ memberList }) => {
+const TeamMembersList = ({memberList}) => {
     return (
-        <div className={`bg-red-100`}>
-            {memberList.map((singleMember) => {
-                return <div className={`grid`}>{singleMember.name}</div>
-            })}
+        <div className={`grid gap-3 pt-5`}>
+            <div className="ml-4 -mt-2">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">Team members</h3>
+            </div>
+            {memberList.map((person) => (
+                <div key={person._id} className="grid grid-cols-5 place-items-center bg-gray-200 rounded-2xl gap-3 mx-4 shadow-lg">
+                    <div className={`grid place-content-center col-span-2 `}>
+                        {person.image ?
+                            <img className="h-14 w-14 rounded-full place-content-center" src={person.image}
+                                 alt=""/> :
+                            <div className="h-14 w-14 rounded-full bg-blue-300 grid place-items-center">
+                                <span
+                                    className="text-3xl font-bold text-gray-800">{person.name.charAt(0).toString().toUpperCase()}</span>
+                            </div>
+                        }
+                    </div>
+                    <div className="ml-3 col-span-2">
+                        <p className="text-base font-medium text-gray-900">{person.name}</p>
+                        <p className="text-sm text-gray-500">{person.email}</p>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
