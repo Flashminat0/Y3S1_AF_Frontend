10a34b19252b40e74e849aa7b088d4ba7cec94a9
swee76 <oshinikithma1213@gmail.com>
2022-05-22 14:31:58 +0530

    Create ProjectTemplateBox.jsx, ProjectTemplateList.jsx, ProjectTemplatesWrapper.jsx

--- components/boxes/lists/group/student/GroupsList.jsx
+++ components/boxes/lists/group/student/GroupsList.jsx
@@ -3,24 +3,28 @@ import GroupSearchWrapper from "../../../../layouts/user/group/student/GroupSear
 import SingleGroupBox from "./SingleGroupBox";
 
 const studentGroupsStaticData = [{
+    id: 1,
     groupName: "Noobcooders",
     groupLeader: "John Smith",
     groupLeaderRegNo: "IT14256789",
     maxNo: 4,
     currentNo: 3
 }, {
+    id: 2,
     groupName: "thorn",
     groupLeader: "Mary Snatiya",
     groupLeaderRegNo: "IT20256749",
     maxNo: 8,
     currentNo: 1
 }, {
+    id: 3,
     groupName: "BugFixers",
     groupLeader: "Jonas Smith",
     groupLeaderRegNo: "IT24156785",
     maxNo: 5,
     currentNo: 3
 }, {
+    id: 4,
     groupName: "darkcodes",
     groupLeader: "Leo Max",
     groupLeaderRegNo: "IT10256350",
@@ -35,8 +39,9 @@ const GroupsList = () => {
     return (
         <div>
             <GroupSearchWrapper>
-                {studentGroups.map((studentGroup) => (
-                    <SingleGroupBox groupName={studentGroup.groupName} groupLeader={studentGroup.groupLeader}
+                {studentGroups.map((studentGroup, index) => (
+                    <SingleGroupBox key={index} groupName={studentGroup.groupName}
+                                    groupLeader={studentGroup.groupLeader}
                                     groupLeaderRegNo={studentGroup.groupLeaderRegNo} maxNo={studentGroup.maxNo}
                                     currentNo={studentGroup.currentNo}/>
                 ))}
