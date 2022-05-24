565515dcec9960f5cdb2c1d2520c9c2ef1987e8b
Flashminat0 <wyasaslowe@gmail.com>
2022-05-24 22:23:33 +0530

    Added chat input field

--- components/chat/RecivedBubble.jsx
+++ components/chat/RecivedBubble.jsx
@@ -5,7 +5,7 @@ const ReceivedBubble = ({content}) => {
         <div className="flex items-end">
             <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
                 <div>
-                    <span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">
+                    <span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600 text-base">
                         Can be verified on any platform using docker
                     </span>
                 </div>
@@ -13,7 +13,7 @@ const ReceivedBubble = ({content}) => {
             <img
                 src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144"
                 alt="My profile"
-                className="w-6 h-6 rounded-full order-1"
+                className="w-8 h-8 rounded-full order-1"
             />
         </div>
     )
