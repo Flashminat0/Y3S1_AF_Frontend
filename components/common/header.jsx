246678ead61df02108d901743fc059913678067d
Flashminat0 <wyasaslowe@gmail.com>
2022-05-25 21:07:35 +0530

    Added chat input field

--- components/common/header.jsx
+++ components/common/header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <div>
             <Image
                 src={
-                    'https://firebasestorage.googleapis.com/v0/b/y3s1-sliit-af.appspot.com/o/Logo%20AF.png?alt=media&token=128d14ab-d90c-4aa2-b021-4c46859ce9aa'
+                    'https://firebasestorage.googleapis.com/v0/b/y3s1-sliit-af.appspot.com/o/Logo%20AF.png?alt=media&token=2abbb496-a605-40b9-8266-4fc5b4ae1cce'
                 }
                 width={250}
                 height={200}
