793b9f7d7341106b335274312b5d406faac0b4ff
Flashminat0 <wyasaslowe@gmail.com>
2022-05-26 14:57:56 +0530

    Receiver chat file bubble

--- components/chat/bubbles/file/RecivedFileBubble.jsx
+++ components/chat/bubbles/file/RecivedFileBubble.jsx
@@ -9,13 +9,13 @@ import {
 const SenderFileBubble = ({file}) => {
     const fileTypes = ['docx', 'pptx', 'xlsx', 'txt', 'pdf', 'jpg', 'png', 'mp4', 'docx', 'mov', 'mp3',]
 
-    return (<div className="flex items-end justify-end group">
-        <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
+    return (<div className="flex items-end">
+        <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
             <div>
                 <Disclosure>
                     {({open}) => (<>
                         <Disclosure.Button
-                            className="flex w-full justify-between rounded-lg rounded-br-none bg-indigo-600 px-4 py-2 text-left text-sm font-medium text-white hover:bg-indigo-700 border-none focus:outline-none focus-visible:ring focus-visible:ring-indigo-600 focus-visible:ring-opacity-75">
+                            className="flex w-full justify-between rounded-lg rounded-bl-none bg-gray-300 text-gray-600 px-4 py-2 text-left text-sm font-medium text-white hover:bg-gray-400 border-none focus:outline-none focus-visible:ring focus-visible:ring-gray-400 focus-visible:ring-opacity-75">
                                     <span className={`text-base`}>
                                         <div
                                             className={`grid grid-cols-10 content-around`}
@@ -66,7 +66,7 @@ const SenderFileBubble = ({file}) => {
                                     </span>
                             <div className={`relative bottom-1 left-2`}>
                                 <RiArrowDropDownLine
-                                    className={`${open ? 'rotate-180 transform' : ''} h-7 w-7 text-white`}
+                                    className={`${open ? 'rotate-180 transform' : ''} h-7 w-7 text-gray-600`}
                                 />
                             </div>
                         </Disclosure.Button>
@@ -101,7 +101,7 @@ const SenderFileBubble = ({file}) => {
         <img
             src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144"
             alt="My profile"
-            className="w-8 h-8 rounded-full order-2"
+            className="w-8 h-8 rounded-full order-1"
         />
     </div>)
 }
