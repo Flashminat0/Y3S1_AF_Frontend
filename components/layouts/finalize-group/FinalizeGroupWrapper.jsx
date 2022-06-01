2c64cfdcb3b8fc5ba1fa6beec56a6d3cfb6784b5
Flashminat0 <wyasaslowe@gmail.com>
2022-06-01 10:52:12 +0530

    all errors are fixed

--- components/layouts/finalize-group/FinalizeGroupWrapper.jsx
+++ components/layouts/finalize-group/FinalizeGroupWrapper.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {BsPersonBoundingBox} from 'react-icons/bs'
 import GreenFullButton from '../../buttons/full-button/GreenFullButton'
+import {useDidUpdate} from '@mantine/hooks'
+import axios from 'axios'
 
-const FinalizeGroupWrapper = ({children, btnFunction}) => {
+const FinalizeGroupWrapper = ({
+    children,
+    btnFunction,
+    groupLeader,
+    groupTopic,
+}) => {
+    const [groupLeaderID, setGroupLeaderID] = useState('')
+    const [groupLeaderName, setGroupLeaderName] = useState('')
+    const [groupLeaderImg, setGroupLeaderImg] = useState('')
+
+    useDidUpdate(async () => {
+        if (groupLeader === '') return
+        await axios
+            .get('/api/users/get-user-data-from-id', {
+                params: {
+                    userId: groupLeader,
+                },
+            })
+            .then((res) => {
+                const leaderName = res.data.name
+
+                setGroupLeaderID(
+                    leaderName
+                        .substring(
+                            leaderName.lastIndexOf(' ') + 1,
+                            leaderName.length
+                        )
+                        .toString()
+                        .toUpperCase()
+                )
+                setGroupLeaderName(
+                    leaderName
+                        .substring(0, leaderName.lastIndexOf(' '))
+                        .toString()
+                        .toUpperCase()
+                )
+                setGroupLeaderImg(res.data.image)
+            })
+    }, [groupLeader])
+
     return (
         <div className={'px-6 py-6 grid grid-cols-1 gap-3'}>
             <GreenFullButton
@@ -13,12 +54,12 @@ const FinalizeGroupWrapper = ({children, btnFunction}) => {
                 <div className={'flex flex-row justify-between items-center'}>
                     <div className={'flex flex-col gap-1'}>
                         <p className={'uppercase text-lg font-semibold'}>
-                            Group Leader
+                            {groupTopic}
                         </p>
                         <div className={'flex flex-row gap-2 items-center'}>
                             <div className={'uppercase text-base'}>Name:</div>
                             <div className={'text-sm text-gray-500'}>
-                                John Smith
+                                {groupLeaderName}
                             </div>
                         </div>
                         <div className={'flex flex-row gap-2 items-center'}>
@@ -26,12 +67,14 @@ const FinalizeGroupWrapper = ({children, btnFunction}) => {
                                 Registered Number:
                             </div>
                             <div className={'text-sm text-gray-500'}>
-                                IT20227089
+                                {groupLeaderID}
                             </div>
                         </div>
                     </div>
                     <div className={'pr-8'}>
-                        <BsPersonBoundingBox
+                        <img
+                            src={groupLeaderImg.url}
+                            alt={groupLeaderImg.name}
                             className={
                                 'w-20 h-20 lg:w-24 lg:h-24 text-gray-600'
                             }
