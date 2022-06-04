76008ee5c7af36a8b9fdf19298f3f4a6003aedab
Tharushi1212 <80015599+Tharushi1212@users.noreply.github.com>
2022-06-04 17:42:12 +0530

    commit1

--- pages/admin/upload-marking-schema.js
+++ pages/admin/upload-marking-schema.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import AdminSideBarWrapper from '../../components/layouts/admin/AdminSideBarWrapper';
+import { useRouter } from 'next/router';
 import Markingschema from '../markingschema/markingschema';
 
 const UploadMarkingSchema = () => {
+    const router = useRouter();
+
+    const openHomePage = async () => {
+        await router.push('/');
+    };
+
     return (
-        <AdminSideBarWrapper selectedPageIndex={4}>
+        <AdminSideBarWrapper selectedPageIndex={4} navigateHome={openHomePage}>
             <Markingschema />
         </AdminSideBarWrapper>
     );
